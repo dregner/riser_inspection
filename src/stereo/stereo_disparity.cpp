c6e28cad6e9b744e757fda2e54e3258b8a89f676
Daniel Regner <daniel_regner@hotmail.com>
2021-10-15 09:49:40 -0300

    disparity calibration using opencgui

--- src/stereo/stereo_disparity.cpp
+++ src/stereo/stereo_disparity.cpp
@@ -17,6 +17,21 @@
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/sync_policies/approximate_time.h>
 
+// initialize values for StereoSGBM parameters
+int numDisparities = 8;
+int blockSize = 5;
+int preFilterType = 1;
+int preFilterSize = 1;
+int preFilterCap = 31;
+int minDisparity = 0;
+int textureThreshold = 10;
+int uniquenessRatio = 15;
+int speckleRange = 0;
+int speckleWindowSize = 0;
+int disp12MaxDiff = -1;
+int dispType = CV_16S;
+// Creating an object of StereoSGBM algorithm
+cv::Ptr<cv::StereoBM> stereo = cv::StereoBM::create();
 
 static const std::string OPENCV_WINDOW_L = "Image L window";
 static const std::string OPENCV_WINDOW_R = "Image R window";
@@ -37,6 +52,22 @@ public:
     ImageConverter()
     {
         initSubscriber(nh_);
+        cv::namedWindow("disparity",cv::WINDOW_NORMAL);
+        cv::resizeWindow("disparity",600,600);
+
+        // Creating trackbars to dynamically update the StereoBM parameters
+        cv::createTrackbar("numDisparities", "disparity", &numDisparities, 18, on_trackbar1);
+        cv::createTrackbar("blockSize", "disparity", &blockSize, 50, on_trackbar2);
+        cv::createTrackbar("preFilterType", "disparity", &preFilterType, 1, on_trackbar3);
+        cv::createTrackbar("preFilterSize", "disparity", &preFilterSize, 25, on_trackbar4);
+        cv::createTrackbar("preFilterCap", "disparity", &preFilterCap, 62, on_trackbar5);
+        cv::createTrackbar("textureThreshold", "disparity", &textureThreshold, 100, on_trackbar6);
+        cv::createTrackbar("uniquenessRatio", "disparity", &uniquenessRatio, 100, on_trackbar7);
+        cv::createTrackbar("speckleRange", "disparity", &speckleRange, 100, on_trackbar8);
+        cv::createTrackbar("speckleWindowSize", "disparity", &speckleWindowSize, 25, on_trackbar9);
+        cv::createTrackbar("disp12MaxDiff", "disparity", &disp12MaxDiff, 25, on_trackbar10);
+        cv::createTrackbar("minDisparity", "disparity", &minDisparity, 25, on_trackbar11);
+
         cv::namedWindow(OPENCV_WINDOW_L);
         cv::namedWindow(OPENCV_WINDOW_R);
 //        cv::namedWindow(OPENCV_WINDOW_D);
@@ -94,19 +125,22 @@ public:
 //        cv::cvtColor(cv_ptr_L->image, imgL_grey, cv::GRAY);
 //        cv::cvtColor(cv_ptr_R->image, imgR_grey, cv::COLOR_BGR2GRAY);
 
-        // Setting parameters for StereoSGBM algorithm
-        int minDisparity = 0.1;
-        int numDisparities = 64;
-        int blockSize = 8;
-        int disp12MaxDiff = 1;
-        int uniquenessRatio = 10;
-        int speckleWindowSize = 10;
-        int speckleRange = 8;
+//        // Setting parameters for StereoSGBM algorithm
+//        int minDisparity = 0.;
+//        int numDisparities = 144;
+//        int preFilterType = 1;
+//        int preFilterSize = 9;
+//        int preFilterCap = 48;
+//        int blockSize = 39;
+//        int disp12MaxDiff = 0;
+//        int uniquenessRatio = 16;
+//        int speckleWindowSize = 24;
+//        int speckleRange = 6;
 
         // Creating an object of StereoSGBM algorithm
-        cv::Ptr<cv::StereoSGBM> stereo = cv::StereoSGBM::create(minDisparity, numDisparities, blockSize,
-                                                                disp12MaxDiff, uniquenessRatio, speckleWindowSize,
-                                                                speckleRange);
+//        cv::Ptr<cv::StereoSGBM> stereo = cv::StereoSGBM::create(minDisparity, numDisparities, blockSize,
+//                                                                disp12MaxDiff, uniquenessRatio, speckleWindowSize,
+//                                                                speckleRange, preFilterCap, preFilterSize, preFilterType);
 
         // Calculating disparith using the StereoSGBM algorithm
         cv::Mat disp, disparity;
@@ -130,13 +164,74 @@ public:
         // Normalizing the disparity map for better visualisation
         cv::normalize(disp, disp, 0, 255, cv::NORM_MINMAX, CV_8UC1); //CV_8UC1
 //        // Displaying the disparity map
-        cv::imshow("OPENCV_WINDOW_D", disp);
+        cv::imshow("disparity", disparity);
 
         // Update GUI Window
         cv::imshow(OPENCV_WINDOW_L, imgL_grey);
         cv::imshow(OPENCV_WINDOW_R, imgR_grey);
         cv::waitKey(3);
     }
+    // Defining callback functions for the trackbars to update parameter values
+
+    static void on_trackbar1( int, void* )
+    {
+        stereo->setNumDisparities(numDisparities*16);
+        numDisparities = numDisparities*16;
+    }
+
+    static void on_trackbar2( int, void* )
+    {
+        stereo->setBlockSize(blockSize*2+5);
+        blockSize = blockSize*2+5;
+    }
+
+    static void on_trackbar3( int, void* )
+    {
+        stereo->setPreFilterType(preFilterType);
+    }
+
+    static void on_trackbar4( int, void* )
+    {
+        stereo->setPreFilterSize(preFilterSize*2+5);
+        preFilterSize = preFilterSize*2+5;
+    }
+
+    static void on_trackbar5( int, void* )
+    {
+        stereo->setPreFilterCap(preFilterCap);
+    }
+
+    static void on_trackbar6( int, void* )
+    {
+        stereo->setTextureThreshold(textureThreshold);
+    }
+
+    static void on_trackbar7( int, void* )
+    {
+        stereo->setUniquenessRatio(uniquenessRatio);
+    }
+
+    static void on_trackbar8( int, void* )
+    {
+        stereo->setSpeckleRange(speckleRange);
+    }
+
+    static void on_trackbar9( int, void* )
+    {
+        stereo->setSpeckleWindowSize(speckleWindowSize*2);
+        speckleWindowSize = speckleWindowSize*2;
+    }
+
+    static void on_trackbar10( int, void* )
+    {
+        stereo->setDisp12MaxDiff(disp12MaxDiff);
+    }
+
+    static void on_trackbar11( int, void* )
+    {
+        stereo->setMinDisparity(minDisparity);
+    }
+
 
 };
 
