9e6e8f2f297eb4c66dd4c0085da5e80b8f72ad35
Daniel Regner <daniel_regner@hotmail.com>
2021-10-21 12:36:42 -0300

    Code simplified

--- include/path_generator.hh
+++ include/path_generator.hh
@@ -11,65 +11,57 @@
 
 #include <iostream>
 #include <fstream>
-#include <math.h>
-#include <stdlib.h>
+#include <cmath>
+#include <cstdlib>
 #include <vector>
 
-
-#define C_PI (double)3.141592653589793
-#define DEG2RAD(DEG) ((DEG) * ((C_PI) / (180.0)))
-#define RAD2DEG(RAD) ((RAD) * (180.0) / (C_PI))
+#define DEG2RAD(DEG) ((DEG) * ((3.141592653589793) / (180.0)))
+#define RAD2DEG(RAD) ((RAD) * (180.0) / (3.141592653589793))
 
 class RiserInspection {
 private:
-    /// Arrays used to store waypoints and initial values
-    float waypointTaskDJI_[8]; // Store DJI waypoint Task parameters
-    double cart_array_[6]; // [x y z dz dy dz]' [6x1]
-    double coord_array_[5]; // [x y z dz dy dz]' [6x1]
+    /// file name to store WP
+    std::ofstream _saved_wp;
+    /// Initialize parameters
+    int altitudeCount_ = 4; // number of horizontal path
+    float deltaAltitude_ = 0.3; // 80% image overlap (CAMERA PARAMETER) TODO: Find a way to bring it from image.
+    int angleCount_ = 5;    // number of vertical path
+    int deltaAngle_ = 15;   // variation of angle for each vertical path
+    float d_cyl_ = 0.3;     // Riser diameters
+    double dist_ = 5;       // Distance between riser and drone
+    int start_angle_ = (-deltaAngle_ * angleCount_ / 2) + deltaAngle_ / 2;
+
+    /// Arrays to store WP creation
+    double polar_array_[2]{}; // Polar array [r deg]
+    double xy_array_[2]{};    // Cart array [x y]
+    double waypoint_[4]{}; // Waypoint array [lat lon alt heading gimbal_pitch]'
 
     /// Initial position to waypoint creates
-    double lat0_ = -27.605299; // Starting latitude
-    double lon0_ = -48.520547; // Starting longitude
-    int alt0_ = 3; // Starting altitude
-    int head0_ = 30; // Starting heading
-    int rotz[3][3];
-    std::ofstream _saved_wp;
+    // TODO: Must come as initialize parameters
+    double lat0_ = -27.605299;  // Starting latitude
+    double lon0_ = -48.520547;  // Starting longitude
+    float alt0_ = 3;              // Starting altitude
+    float head0_ = 30;            // Starting heading
+
+    /// Internal parameters
+    int Y_North_ZeroDegrees_ = -90;
 public:
     RiserInspection();
 
     ~RiserInspection();
 
-    void setInitCoord(double lon, double lat, int alt, int head);
+    void setInitCoord(double dist, float d_cyl, double lon, double lat, float alt, float head);
 
-    void setDJIwaypointTask(float velocity_range, float idle_velocity, int action_on_finish,
-                            int mission_exec_times, int yaw_mode, int trace_mode,
-                            int action_on_rc_lost, int gimbal_pitch_mode);
+    void inspectionAngletoHeading(float polar_angle);
 
-    void get_gps_position(const sensor_msgs::NavSatFixConstPtr &msg_gps, const sensor_msgs::NavSatFixConstPtr &msg_rtk);
+    void polar2cart(double r, double alpha);
 
-    void rotz_cartP(int yaw);
+    void cart2gcs(double altitude);
 
-    /** @param wp_array Array to be ploted
-        @param size Size of array to be ploted (normaly 5 or 6)
-        @return printed waypoint at terminal*/
-    void print_wp(double *wp_array, int size, int n);
+    void findCenterHeading(int deltaAngle, int angleCount);
+
+    void createInspectionPoints();
 
     void csv_save_wp(double *wp_array, int row);
-
-    /** @param cart_wp Array of 6 elements provided from cartesian [x y z dz dy dz]'
-        @return coord_wp - Lat, lon, alt, roll (north heading) and pitch (gimbal) [Nx5] */
-
-    void pointCartToCord(double cart_wp[6], int nCount);
-
-    /** @param phi  Riser diameter
-        @param d    Distance to riser wall
-        @param da   Angle delta. Degrees
-        @param nh   Number of acquisitions on same height
-        @param dv   Delta height (altitude or Z)
-        @param nv   Number of acquisitions levels
-        @return Format: [x y z dz dy dz]' [6x1]  */
-
-    void createInspectionPoints(const double phi, const float d, const float da,
-                                const float nh, const float dv, const float nv);
 
 };
--- src/path/path_generator.cpp
+++ src/path/path_generator.cpp
@@ -9,54 +9,41 @@ RiserInspection::~RiserInspection() {
     _saved_wp.close();
 }
 
-
-
-void RiserInspection::setInitCoord(double lon, double lat, int alt, int head) {
+void RiserInspection::setInitCoord(double dist, float d_cyl, double lon, double lat, float alt, float head) {
+    d_cyl_ = d_cyl;
+    dist_ = dist;
     lon0_ = lon;
     lat0_ = lat;
     alt0_ = alt;
     head0_ = head;
 }
 
-void RiserInspection::setDJIwaypointTask(float velocity_range, float idle_velocity, int action_on_finish,
-                                         int mission_exec_times, int yaw_mode, int trace_mode,
-                                         int action_on_rc_lost, int gimbal_pitch_mode) {
-    waypointTaskDJI_[0] = velocity_range;
-    waypointTaskDJI_[1] = idle_velocity;
-    waypointTaskDJI_[2] = action_on_finish;
-    waypointTaskDJI_[3] = mission_exec_times;
-    waypointTaskDJI_[4] = yaw_mode;
-    waypointTaskDJI_[5] = trace_mode;
-    waypointTaskDJI_[6] = action_on_rc_lost;
-    waypointTaskDJI_[7] = gimbal_pitch_mode;
+void RiserInspection::inspectionAngletoHeading(float polar_angle) {
+
+    float heading = -polar_angle + head0_;
+    /// Conditions to keep range - 180 to 180
+    if (heading < -180) { heading = heading - 360; }
+    if (heading > 180) { heading = heading + 360; }
+    waypoint_[3] = heading;
+}
+
+void RiserInspection::polar2cart(double r, double alpha) {
+    xy_array_[0] = r * cos(DEG2RAD(alpha));
+    xy_array_[1] = r * sin(DEG2RAD(alpha));
 
 }
 
-void RiserInspection::rotz_cartP(int yaw) {
-
-    rotz[0][0] = (int) cos(DEG2RAD(yaw));
-    rotz[0][1] = (int) (sin(DEG2RAD(yaw)) * -1);
-    rotz[0][2] = 0;
-
-    rotz[1][0] = (int) sin(DEG2RAD(yaw));
-    rotz[1][1] = (int) cos(DEG2RAD(yaw));
-    rotz[1][2] = 0;
-
-    rotz[2][0] = 0;
-    rotz[2][1] = 0;
-    rotz[2][2] = 1;
+void RiserInspection::cart2gcs(double altitude) {
+    int R = 6371000; // Earth radius
+    double x0 = R * DEG2RAD(lon0_) * cos(DEG2RAD(lon0_)); // Initial Position X
+    double y0 = R * DEG2RAD(lat0_); // Initial Position Y
+    double x = xy_array_[0] + x0;
+    double y = xy_array_[1] + y0;
+    waypoint_[0] = RAD2DEG(y / R);
+    waypoint_[1] = RAD2DEG(x / (R * cos(DEG2RAD(lon0_))));
+    waypoint_[2] = altitude;
 }
 
-
-void RiserInspection::print_wp(double *wp_array, int size, int n) {
-    char const *cartesian[6] = {"x", "y", "z", "dz", "dy", "dz"};
-    char const *gns_cord[5] = {"lat", "lon", "alt", "roll", "pitch"};
-    std::cout << "Waypoint - " << n << std::endl;
-    for (int i = 0; i < size; i++) {
-        if (size > 5) { std::cout << cartesian[i] << "\t" << wp_array[i] << std::endl; }
-        else { std::cout << gns_cord[i] << "\t" << wp_array[i] << std::endl; }
-    }
-}
 
 void RiserInspection::csv_save_wp(double *wp_array, int row) {
     for (int i = 0; i < row; i++) {
@@ -67,148 +54,44 @@ void RiserInspection::csv_save_wp(double *wp_array, int row) {
     }
 }
 
-void RiserInspection::pointCartToCord(double cart_wp[6], int nCount) {
-
-    //[6371 km]. the approximate radius of earth
-    long R = 6371 * 1000;
-
-
-
-    // Convert starting coordinate (lat lon alt) to cartesian (XYZ)
-    double x0 = R * DEG2RAD(lon0_) * cos(DEG2RAD(lon0_));
-    double y0 = R * DEG2RAD(lat0_);
-    double z0 = (cart_array_[2] * -1) + alt0_;
-
-
-    double firstHeading = 0;
-
-    double x = cart_array_[0] + x0;
-    double y = cart_array_[1] + y0;
-    double alt = cart_array_[2] + z0;
-
-
-    double lon = RAD2DEG(x / (R * cos(DEG2RAD(lon0_))));
-    double lat = RAD2DEG(y / R);
-
-    coord_array_[0] = lat;
-    coord_array_[1] = lon;
-    coord_array_[2] = alt;
-
-
-    double roll = RAD2DEG(atan2(cart_wp[4], cart_wp[3]));
-
-    double heading = roll - 180 - 90;
-
-    if (nCount == 0) {
-        firstHeading = heading;
-    }
-
-    double pitch = DEG2RAD(asin(cart_array_[5]));
-
-    heading = (heading - firstHeading + head0_);
-
-    if (heading < -180) {
-        heading = heading + 360;
-    } else if (heading > 180) {
-        heading = heading - 360;
-    }
-//
-    coord_array_[3] = heading;
-    coord_array_[4] = pitch;
+void RiserInspection::findCenterHeading(int deltaAngle, int angleCount) {
+    if (angleCount % 2 == 1) { start_angle_ = (-deltaAngle * angleCount / 2) + deltaAngle / 2; }
+    else { start_angle_ = (int) (-angleCount * round(angleCount / 2)); }
 }
 
-void RiserInspection::createInspectionPoints(const double phi, const float d, const float da, const float nh,
-                                             const float dv, const float nv) {
-    // Inspection radius.
-    double r = d + phi / 2;
-
-    int nCount = 0;
-    float p[3] = {0, 0, 0};
-
-    // Height change
-    float hc = ((nv - 1) * dv);
-
-    //Path control
-    bool isVertical = true;
-
-    // Position [XY] - Horizontal levels
-
-    for (int i = 0; i <= nh - 1; i++) {
-        // Update Alpha
-        float alpha = i * da;
-        float hs;
-        float hl;
-
-        //Controls vertical path
-        if (isVertical) {
-            hl = 0; // Initial height
-            hs = dv; // Height step
-        } else {
-            hl = hc; // Initial height
-            hs = dv * (-1); // Initial step
+void RiserInspection::createInspectionPoints() {
+    findCenterHeading(deltaAngle_, angleCount_);
+//    double *anglesArray = new double[angleCount_];
+    float initial = alt0_; // initiate altitude value
+    float vertical; // Set if will move drone down or up INITIALLY DOWN.
+    for (int i = 0; i < angleCount_; i++) {
+        if(i%2 == 1){ vertical = -1;} else{vertical = 1;}
+        for (int k = 0; k < altitudeCount_; k++) {
+            /// Set altitude of this waypoint
+            float altitude = initial + (float) k*vertical*deltaAltitude_;
+            /// Set Polar values
+            polar_array_[0] = dist_ + d_cyl_ / 2; // distance riser and drone
+            polar_array_[1] = start_angle_ + i * deltaAngle_; // angle of inspection r^angle (Polar)
+            polar_array_[1] += head0_ - 90; // Compense heading orientation and -90 to transform N to 0 deg
+            /// Convert Polar to Cartesian
+            polar2cart(polar_array_[0], polar_array_[1]);
+            /// Introduce values to waypoint array to be printed
+            cart2gcs(altitude);
+            inspectionAngletoHeading((float) polar_array_[1]);
+            /// Export to CSV file
+            csv_save_wp(waypoint_, sizeof(waypoint_) / sizeof(waypoint_[0]));
+            if(k == altitudeCount_-1){initial = altitude;} // set initial altitude value when finished de vertical movement.
         }
-
-        for (int j = 0; j <= nv - 1; j++) {
-            float z = j * hs + hl;
-
-            // Lines below set position
-            float x = r * cos(DEG2RAD(alpha));
-            float y = r * sin(DEG2RAD(alpha));
-            p[0] = x;
-            p[1] = y;
-            p[2] = z;
-
-            // Orientation
-            float endPoint[3] = {0, 0, z};
-
-            //TODO: Verificar com o Pedro a sutração dos arrays (matrizes)
-            double dr[3] = {endPoint[0] - x, endPoint[1] - y, endPoint[2] - z};
-
-            // Calculation of the absolute value of the array
-            float absolute = sqrt((dr[0] * dr[0]) + (dr[1] * dr[1]) + (dr[2] * dr[2]));
-
-            dr[0] = dr[0] / absolute;
-            dr[1] = dr[1] / absolute;
-            dr[2] = dr[2] / absolute;
-
-            RiserInspection::rotz_cartP(180);
-
-            for (int j = 0; j < sizeof(rotz) / sizeof(rotz[0]); j++) { // j from 0 to 2
-                double var_p = 0, var_dr =0;
-                for (int k = 0; k < sizeof(rotz[0]) / sizeof(int); k++) { // k from 0 to 2
-                    var_p += (rotz[j][k] * p[j]);
-                    var_dr += (rotz[j][k] * dr[j]);
-                    if(k == 2){
-                        p[j] = var_p;
-                        dr[j] = var_dr;}
-                }
-            }
-
-            cart_array_[0] = p[0];
-            cart_array_[1] = p[1];
-            cart_array_[2] = p[2]+alt0_;
-            cart_array_[3] = dr[0];
-            cart_array_[4] = dr[1];
-            cart_array_[5] = dr[2];
-            RiserInspection::pointCartToCord(cart_array_, nCount);
-
-//            print_wp(cart_array_, sizeof(cart_array_) / sizeof(cart_array_[0]), nCount);
-//            print_wp(coord_array_, sizeof(coord_array_) / sizeof(coord_array_[0]), nCount);
-            csv_save_wp(coord_array_, sizeof(coord_array_) / sizeof(coord_array_[0]));
-//            csv_save_wp(cart_array_, sizeof(cart_array_) / sizeof(cart_array_[0]));
-            nCount++;
-        }
-        isVertical = !isVertical;
     }
 }
-
 
 int main() {
     RiserInspection riser;
-    std::cout << "Create waypoint pathway" << std::endl;
+    std::cout << "Creating waypoint pathway" << std::endl;
 
-    riser.setInitCoord(-48.520547, -27.605299, 10, 30);
-    riser.createInspectionPoints(0.3, 5, 15, 5, -0.3, 10);
+    riser.setInitCoord(5, 0.3, -48.520547, -27.605299, 10, 30);
+    riser.createInspectionPoints();
+    std::cout << "Finished" << std::endl;
 
     return 0;
 }
